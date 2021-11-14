640152374778a1c830e67290954a20dac3f67795
IlliaC09 <i6991063@gmail.com>
2021-11-14 18:25:16 +0100

    Домашнее задание урок 13

---
+++ HomeWork/Lessons13/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //  TextFormatterB.removeMiddleChars("road");
+        // TextFormatterB.findWord();
+        // TextFormatterB.findJava();
+        //  TextFormatterB.middleOfString("London is the capital of Great Britain");
+        TextFormatterB.findEnglishWord();
+    }
+}
